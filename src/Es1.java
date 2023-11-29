e1aaac9a09450258ba91b7831aba39f123197f31
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-11-29 17:37:49 +0100

    md es1

--- src/Es1.java
+++ src/Es1.java
@@ -1,7 +1,4 @@
 import Rectangle.Rettangolo;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Es1 {
 
     public static void printRettangolo(Rettangolo rectangle){
